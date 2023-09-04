4f460373e25c28dc85c5c32ab7024fab2e0976d7
Jackson Barreto <jackson.barreto@gmail.com>
2023-09-04 17:28:13 +0200

    feat: auth routes

---
+++ internal/app/router/routes.go
@@ -0,0 +1,43 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"idp-automations-hub/internal/app/authentication"
+	"idp-automations-hub/internal/app/config"
+)
+
+func initializeRoutes(router *gin.Engine) error {
+	relativePathV1 := config.ServerConfig.BaseURL + "/v1"
+	v1 := router.Group(relativePathV1)
+	{
+		// initialize auth routes
+		err := initializeAuthRoutes(v1)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func initializeAuthRoutes(apiVersion *gin.RouterGroup) error {
+	authService, err := authentication.GetDefaultAuthService()
+	if err != nil {
+		return err
+	}
+	auth := apiVersion.Group("/auth")
+	{
+		// initialize auth routes
+		authHandler := authentication.NewHandler(authService)
+		auth.POST("/register", authHandler.Register)
+		auth.POST("/login", authHandler.Login)
+		auth.POST("/logout", authHandler.Logout)
+		auth.POST("/refresh-token", authHandler.RefreshToken)
+		auth.POST("/request-password-reset", authHandler.RequestPasswordReset)
+		auth.GET("/confirm-password-reset/:reset-token", authHandler.ConfirmPasswordReset)
+		auth.POST("/change-password", authHandler.ChangePassword)
+		auth.POST("/is-user-authenticated", authHandler.IsUserAuthenticated)
+	}
+
+	return nil
+}
